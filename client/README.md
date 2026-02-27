976f0c6958d625ad27472dd03dbd34ae3f7bacc6
Kasper Aaquist Pihl Johansen <kasperaaquist@gmail.com>
2026-02-27 13:58:13 +0100

    fix: trying to add hitl... but I fail
    Signed-off-by: Kasper Aaquist Pihl Johansen <kasperaaquist@gmail.com>

--- client/README.md
+++ client/README.md
@@ -7,15 +7,7 @@ To run the client:
 ```text
 uv run chainlit run src/client/client.py -w
 ```
-In the .env file I have added a OpenAI key. 
-When running the Agent Supervisor I did not manage to get a local Ollama model to work. Therefor I use the OpenAI for the Supervisor.
 
-
-### create the `.env` file
-```text
-cat .env
-OPENAI_API_KEY=sk-.... 
-```
 
 <!--## Images of Chainlit
 | ![Running Client w supervisor math agent](images/math_agent.png "output evaluate tool") |
--- client/src/client/client.py
+++ client/src/client/client.py
@@ -1,23 +1,24 @@
+from tarfile import version
+
 from langchain_core.runnables import RunnableConfig
 from langchain_ollama import ChatOllama
 from langgraph_supervisor import create_supervisor
 from langchain.agents import create_agent
 from langchain.agents.middleware import HumanInTheLoopMiddleware
-from langgraph.checkpoint.memory import InMemorySaver
-from langgraph.store.memory import InMemoryStore
+from langgraph.checkpoint.memory import MemorySaver
 
 from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.client import MultiServerMCPClient
 import asyncio
+import logging
 
 import chainlit as cl
 
+logger = logging.getLogger("__NAME__")
 
 # Create a supervisor graph
 async def create_multi_agent_graph():
     """Create a LangGraph graph with a supervisor and MCP servers."""
-    checkpointer = InMemorySaver()
-    store = InMemoryStore()
     ollama_chat_llm = ChatOllama(
         base_url="http://localhost:11435",
         model="llama3.1:8b",
@@ -37,9 +38,11 @@ async def create_multi_agent_graph():
         }
     )
     search_tools = await client.get_tools()
+    # for t in search_tools:
+    #     logger.info(f"!!!! {t.get_name()}")
     search_agent = create_agent(
         model=ollama_chat_llm,
-        tools=[search_tools],
+        tools=search_tools,
         system_prompt=(
             "You are a medical research agent. \n\n"
             "INSTRUCTIONS:\n"
@@ -53,14 +56,18 @@ async def create_multi_agent_graph():
         middleware=[
             HumanInTheLoopMiddleware(
                 interrupt_on={
-                    "search_tools": {"allowed_decisions": ["approve", "reject"]}},
-                # Prefix for interrupt messages - combined with tool name and args to form the full message
-                # e.g., "Tool execution pending approval: execute_sql with query='DELETE FROM...'"
-                # Individual tools can override this by specifying a "description" in their interrupt config
+                    "search_tools": {"allowed_decisions": ["approve", "reject"]},
+                    "pubmed_article_connections": {"allowed_decisions": ["approve", "reject"]},
+                    "pubmed_fetch_contents": {"allowed_decisions": ["approve", "reject"]},
+                    "pubmed_generate_chart": {"allowed_decisions": ["approve", "reject"]},
+                    "pubmed_research_agent": {"allowed_decisions": ["approve", "reject"]},
+                    "pubmed_search_articles": {"allowed_decisions": ["approve", "reject"]},
+                    "search_google_scholar": {"allowed_decisions": ["approve", "reject"]},
+                },
                 description_prefix="Tool execution pending approval",
             ),
         ],
-        checkpointer=checkpointer,
+        checkpointer=MemorySaver()
     )
 
     # Create the supervisor
@@ -75,10 +82,24 @@ async def create_multi_agent_graph():
             "- Do formatting of the text or other related queries related to formatting of the results"
             "- Don't do any other work yourself."
         ),
-        add_handoff_back_messages=False,
+        # middleware=[
+        #     HumanInTheLoopMiddleware(
+        #         interrupt_on={
+        #             "search_tools": {"allowed_decisions": ["approve", "reject"]},
+        #             "pubmed_article_connections": {"allowed_decisions": ["approve", "reject"]},
+        #             "pubmed_fetch_contents": {"allowed_decisions": ["approve", "reject"]},
+        #             "pubmed_generate_chart": {"allowed_decisions": ["approve", "reject"]},
+        #             "pubmed_research_agent": {"allowed_decisions": ["approve", "reject"]},
+        #             "pubmed_search_articles": {"allowed_decisions": ["approve", "reject"]},
+        #             "search_google_scholar": {"allowed_decisions": ["approve", "reject"]},
+        #         },
+        #         description_prefix="Tool execution pending approval",
+        #     ),
+        # ],
+        #add_handoff_back_messages=False,
         output_mode="last_message",
     )
-    return supervisor.compile(checkpointer=checkpointer, store=store)
+    return supervisor.compile(checkpointer=MemorySaver())
 
 
 @cl.on_message
@@ -86,14 +107,29 @@ async def run(msg: cl.Message):
     graph = await create_multi_agent_graph()
 
     config = {"configurable": {"thread_id": cl.context.session.id}}
+    graph_state = await graph.aget_state(config={"configurable": {"thread_id": cl.context.session.id}})
+    if graph_state:
+        historic_messages = graph_state.values.get("messages", [])
+    else:
+        historic_messages = []
+    historic_messages.append(msg.content)
     cb = cl.LangchainCallbackHandler()
     final_answer = cl.Message(content="")
-    async for messages, metadata in graph.astream({"messages": [HumanMessage(content=msg.content)]}, stream_mode="messages", config=RunnableConfig(callbacks=[cb], **config)):
+    async for metadata, mode, chunk in graph.astream({"messages": historic_messages}, stream_mode=["updates", "messages"], subgraphs=True, config=RunnableConfig(callbacks=[cb], **config)):
+        logger.info(f"!!!! mode: {mode} \n\n chunk: {chunk} \n\n\n\n")
         if (
-                messages.content
-                and not isinstance(messages, HumanMessage)
+                mode == "messages"
+                #and not isinstance(messages, HumanMessage)
         ):
-            await final_answer.stream_token(messages.content)
+            token, _ = chunk
+            if not isinstance(token, HumanMessage):
+                await final_answer.stream_token(token.content)
+        elif mode == "updates":
+            # Check for interrupt
+            if "__interrupt__" in chunk:
+                logger.info(f"!!!!! \n\n\n\nInterrupt: {chunk['__interrupt__']}\n\n\n\n\n")
+
+
 
     await final_answer.send()
 
