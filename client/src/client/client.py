9986d39d557ae0edf07bb65d2396eb8b12b1b21f
Kasper Aaquist Johansen <kasperaaquist@gmail.com>
2026-03-10 07:10:43 +0100

    fix: added state
    Signed-off-by: Kasper Aaquist Johansen <kasperaaquist@gmail.com>

--- client/src/client/client.py
+++ client/src/client/client.py
@@ -3,10 +3,13 @@ from tarfile import version
 from langchain_core.runnables import RunnableConfig
 from langchain_ollama import ChatOllama
 from langgraph_supervisor import create_supervisor
+from deepagents.middleware.subagents import CompiledSubAgent
 from deepagents import create_deep_agent
+
 from langchain.agents import create_agent
 from langchain.agents.middleware import HumanInTheLoopMiddleware
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.memory import MemorySaver
+
 
 from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.client import MultiServerMCPClient
@@ -41,68 +44,98 @@ async def create_multi_agent_graph():
     search_tools = await client.get_tools()
     # for t in search_tools:
     #     logger.info(f"!!!! {t.get_name()}")
-    search_agent = create_agent(
-        model=ollama_chat_llm,
-        tools=search_tools,
-        system_prompt=(
-            "You are a medical research agent. \n\n"
-            "INSTRUCTIONS:\n"
-            "- Assist ONLY with research related tasks, DO NOT do anything else\n"
-            "- After you're done with your tasks,  respond to the supervisor directly\n"
-            "- Respond ONLY with the results of your work\n"
-            "- Add a tag to the message so the source can be traced\n"
-            "- do NOT include ANY other text."
-        ),
-        name="search_agent",
-        middleware=[
-            HumanInTheLoopMiddleware(
-                interrupt_on={
-                    "search_tools": {"allowed_decisions": ["approve", "reject"]},
-                    "pubmed_article_connections": {"allowed_decisions": ["approve", "reject"]},
-                    "pubmed_fetch_contents": {"allowed_decisions": ["approve", "reject"]},
-                    "pubmed_generate_chart": {"allowed_decisions": ["approve", "reject"]},
-                    "pubmed_research_agent": {"allowed_decisions": ["approve", "reject"]},
-                    "pubmed_search_articles": {"allowed_decisions": ["approve", "reject"]},
-                    "search_google_scholar": {"allowed_decisions": ["approve", "reject"]},
-                },
-                description_prefix="Tool execution pending approval",
-            ),
-        ],
-        checkpointer=InMemorySaver()
-    )
+    # search_agent = create_agent(
+    #     model=ollama_chat_llm,
+    #     tools=search_tools,
+    #     system_prompt=(
+    #         "You are a medical research agent. \n\n"
+    #         "INSTRUCTIONS:\n"
+    #         "- Assist ONLY with research related tasks, DO NOT do anything else\n"
+    #         "- After you're done with your tasks,  respond to the supervisor directly\n"
+    #         "- Respond ONLY with the results of your work\n"
+    #         "- Add a tag to the message so the source can be traced\n"
+    #         "- do NOT include ANY other text."
+    #     ),
+    #     name="search_agent",
+    #     middleware=[
+    #         HumanInTheLoopMiddleware(
+    #             interrupt_on={
+    #                 "search_tools": {"allowed_decisions": ["approve", "reject"]},
+    #                 "pubmed_article_connections": {"allowed_decisions": ["approve", "reject"]},
+    #                 "pubmed_fetch_contents": {"allowed_decisions": ["approve", "reject"]},
+    #                 "pubmed_generate_chart": {"allowed_decisions": ["approve", "reject"]},
+    #                 "pubmed_research_agent": {"allowed_decisions": ["approve", "reject"]},
+    #                 "pubmed_search_articles": {"allowed_decisions": ["approve", "reject"]},
+    #                 "search_google_scholar": {"allowed_decisions": ["approve", "reject"]},
+    #             },
+    #             description_prefix="Tool execution pending approval",
+    #         ),
+    #     ],
+    #     checkpointer=MemorySaver()
+    # )
 
     # Create the supervisor
-    supervisor = create_supervisor(
-        agents=[search_agent],
+    # supervisor = create_supervisor(
+    #     agents=[search_agent],
+    #     model=ollama_chat_llm,
+    #     prompt=(
+    #         "You are a supervisor managing one agent:\n"
+    #         "- a search agent.\n"
+    #         "INSTRUCTIONS:\n"
+    #         "- Assign search of article and medical abstract related tasks to the search agent\n"
+    #         "- Do formatting of the text or other related queries related to formatting of the results"
+    #         "- Don't do any other work yourself."
+    #     ),
+    #     # middleware=[
+    #     #     HumanInTheLoopMiddleware(
+    #     #         interrupt_on={
+    #     #             "search_tools": {"allowed_decisions": ["approve", "reject"]},
+    #     #             "pubmed_article_connections": {"allowed_decisions": ["approve", "reject"]},
+    #     #             "pubmed_fetch_contents": {"allowed_decisions": ["approve", "reject"]},
+    #     #             "pubmed_generate_chart": {"allowed_decisions": ["approve", "reject"]},
+    #     #             "pubmed_research_agent": {"allowed_decisions": ["approve", "reject"]},
+    #     #             "pubmed_search_articles": {"allowed_decisions": ["approve", "reject"]},
+    #     #             "search_google_scholar": {"allowed_decisions": ["approve", "reject"]},
+    #     #         },
+    #     #         description_prefix="Tool execution pending approval",
+    #     #     ),
+    #     # ],
+    #     #add_handoff_back_messages=False,
+    #     output_mode="last_message",
+    # )
+
+    # search_subagent = {
+    #     "name": "search-agent",
+    #     "description": "Used to search more in depth questions",
+    #     "system_prompt": (
+    #             "You are a medical research agent. \n\n"
+    #             "INSTRUCTIONS:\n"
+    #             "- Assist ONLY with research related tasks, DO NOT do anything else\n"
+    #             "- After you're done with your tasks,  respond to the supervisor directly\n"
+    #             "- Respond ONLY with the results of your work\n"
+    #             "- Add a tag to the message so the source can be traced\n"
+    #             "- do NOT include ANY other text."
+    #         ),
+    #     "tools": search_tools,
+    #     "model": ollama_chat_llm,  # Optional override, defaults to main agent model
+    # }
+
+    compiled_subagent = create_agent(
         model=ollama_chat_llm,
-        prompt=(
-            "You are a supervisor managing one agent:\n"
-            "- a search agent.\n"
-            "INSTRUCTIONS:\n"
-            "- Assign search of article and medical abstract related tasks to the search agent\n"
-            "- Do formatting of the text or other related queries related to formatting of the results"
-            "- Don't do any other work yourself."
-        ),
-        # middleware=[
-        #     HumanInTheLoopMiddleware(
-        #         interrupt_on={
-        #             "search_tools": {"allowed_decisions": ["approve", "reject"]},
-        #             "pubmed_article_connections": {"allowed_decisions": ["approve", "reject"]},
-        #             "pubmed_fetch_contents": {"allowed_decisions": ["approve", "reject"]},
-        #             "pubmed_generate_chart": {"allowed_decisions": ["approve", "reject"]},
-        #             "pubmed_research_agent": {"allowed_decisions": ["approve", "reject"]},
-        #             "pubmed_search_articles": {"allowed_decisions": ["approve", "reject"]},
-        #             "search_google_scholar": {"allowed_decisions": ["approve", "reject"]},
-        #         },
-        #         description_prefix="Tool execution pending approval",
-        #     ),
-        # ],
-        #add_handoff_back_messages=False,
-        output_mode="last_message",
+        tools=search_tools,
+        name="search-agent",
+        system_prompt=(
+                    "You are a medical research agent. \n\n"
+                    "INSTRUCTIONS:\n"
+                    "- Assist ONLY with research related tasks, DO NOT do anything else\n"
+                    "- After you're done with your tasks,  respond to the supervisor directly\n"
+                    "- Respond ONLY with the results of your work\n"
+                    "- Add a tag to the message so the source can be traced\n"
+                    "- do NOT include ANY other text."
+                ),
     )
 
-    create_deep_agent(
-        model=ollama_chat_llm,
+    supervisor = create_deep_agent(
         system_prompt=(
             "You are a supervisor managing one agent:\n"
             "- a search agent.\n"
@@ -111,6 +144,22 @@ async def create_multi_agent_graph():
             "- Do formatting of the text or other related queries related to formatting of the results"
             "- Don't do any other work yourself."
         ),
+        subagents=[
+            CompiledSubAgent(
+                name="approval-agent",
+                description="An agent that can request approvals",
+                runnable=compiled_subagent,
+            ),{
+                "interrupt_on": {
+                "search_tools": {"allowed_decisions": ["approve", "reject"]},
+                "pubmed_article_connections": {"allowed_decisions": ["approve", "reject"]},
+                "pubmed_fetch_contents": {"allowed_decisions": ["approve", "reject"]},
+                "pubmed_generate_chart": {"allowed_decisions": ["approve", "reject"]},
+                "pubmed_research_agent": {"allowed_decisions": ["approve", "reject"]},
+                "pubmed_search_articles": {"allowed_decisions": ["approve", "reject"]},
+                "search_google_scholar": {"allowed_decisions": ["approve", "reject"]},
+                }}
+        ],
 
     )
     return supervisor.compile(checkpointer=InMemorySaver())
